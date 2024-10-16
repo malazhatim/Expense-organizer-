b184c1d4b04130ccf2b0ed5e29dcb997ce5eec00
Esra-333 <140227176+Esra-333@users.noreply.github.com>
2024-10-15 20:10:02 -0700

    Update and delete total_income.php

--- total_income.php
+++ total_income.php
@@ -1,8 +1,8 @@
 <?php
 // Database connection
 $servername = "localhost";
-$username = "root"; // use your database username
-$password = ""; // use your database password
+$username = "root"; 
+$password = ""; 
 $dbname = "income_db";
 
 // Create connection
